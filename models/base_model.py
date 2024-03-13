8383c3948d25ed716a0e54c0c4048b643f658625
foultrip <davidvasquezmahecha@gmail.com>
2024-03-13 11:07:05 -0500

    correccion de consola al crear un nuevo BaseModel con FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
