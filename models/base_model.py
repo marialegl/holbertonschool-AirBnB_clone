f31efcc9a2ab9c5fc8579c84f22780fc9ed0ee63
foultrip <davidvasquezmahecha@gmail.com>
2024-03-12 21:24:55 -0500

    correccion base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import uuid
 import datetime
 
@@ -9,15 +11,14 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        print f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-
-        self.__dict__.__setitem__('__class__', self.__class__.__name__)
-
-        return dict(self.__dict__)
+        dictCopy = self.__dict__.copy()
+        dictCopy["__class__"] = self.__class__.__name__
+        dictCopy["created_at"] = self.created_at.isoformat()
+        dictCopy["updated_at"] = self.updated_at.isoformat()
+        return dictCopy
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-import unitest
-import pycodestyle
+
+import unittest
 from models.base_model import BaseModel
 
-class Test_base_model(unitest.TestCase):
+
+class Test_base_model(unittest.TestCase):
     def unique_id(self):
         instance1 = BaseModel()
         instance2 = BaseModel()
         self.assertNotEqual(instance1, instance2)
+
+
+if __name__ == "__main__":
+    unittest.main()
