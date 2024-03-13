20227536a846960e100eb48bdfd709b71dac9c91
marialegl <148149112+marialegl@users.noreply.github.com>
2024-03-13 01:40:21 +0000

    Add files

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
+import unitest
+import pycodestyle
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class Test_base_model(unitest.TestCase):
+    def unique_id(self):
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        self.assertNotEqual(instance1, instance2)
