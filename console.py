7925367ca83a360799d55d13984a34497350b4a7
foultrip <davidvasquezmahecha@gmail.com>
2024-03-13 11:43:56 -0500

    corerccion consola

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from cmd import Cmd
+from models import storage
 from models.base_model import BaseModel
 
 
@@ -25,6 +26,36 @@ class HBNBCommand(Cmd):
             newInst = eval(className)()
             newInst.save()
             print(newInst.id)
+
+    def do_show(self, arg):
+        """
+        prints the string representation of an instance based on className and id
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] is not HBNBCommand.class_list:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            class_name = args[0]
+            key = class_name + "." + args[1]
+            allObjs = storage.all()
+            if key in allObjs:
+                print(allObjs[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        pass
+
+    def do_all(self, arg):
+        pass
+
+    def do_update(self, arg):
+        pass
 
 
 if __name__ == "__main__":
